c0bac51699aeff45e90769ff1a49e183cafb7637
Saikat Das <saikatdas0790@gmail.com>
2025-12-14 14:37:56 +0100

    Add Longhorn disk configs, create Longhorn dirs/files, update install ignition and remove base.bu
    - Add nvme1n1 disk partitioning for Longhorn to control-plane and worker
      Butane configs
    - Create /etc/longhorn directories and Longhorn default config (worker)
      and fstab placeholders (control-plane & worker)
    - Update control-plane and worker readiness services to reference
      Longhorn device by label
    - Change hetzner-rescue script to use control-plane.ign
    - Remove reference base.bu (deleted)

--- hetzner-rescue.sh
+++ hetzner-rescue.sh
@@ -45,6 +45,6 @@ podman run --pull=always --privileged --rm \
   -v /dev:/dev -v /run/udev:/run/udev -v $(pwd):/data -w /data \
   quay.io/coreos/coreos-installer:release \
   install /dev/nvme0n1 \
-  --ignition-file install.ign \
+  --ignition-file control-plane.ign \
   --stream stable \
   --architecture x86_64
